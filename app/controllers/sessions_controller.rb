818eebb8cd7b0d50c6968e548ec5430b0189ca91
Xinxin Xue <xxue@zoology.ubc.ca>
2017-03-01 19:10:20 -0800

    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email params[:email]
-    if user&.authenticate(params[:password])
+    if user&&authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: "Signed in!"
     else
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
       render :new
     end
   end
-    # 
+    #
     # def edit
     #   # session = session[:user_id]
     #   # session[:user_id] = current_user.id
